896a46045d7ba3cdc3e1d712c12222bb426f1a57
Sandro Saab <ss298346@falmouth.ac.uk>
2024-03-18 19:39:04 +0000

    Add offset to planting location

--- Guarden Test/Assets/Scripts/Player/Inventory.cs
+++ Guarden Test/Assets/Scripts/Player/Inventory.cs
@@ -4,25 +4,39 @@ using UnityEngine.InputSystem;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject plantLocation;
+
     private List<GameObject> items = new List<GameObject>();
     private int selectedItem = 0;
     private GameObject currentPlant;
 
     public void OnPlant(InputAction.CallbackContext context)
     {
+        if(context.started)
+        {
+            plantLocation.SetActive(true);
+        }
+
         if (context.performed)
         {
             Debug.Log("ON PLANT");
+            plantLocation.SetActive(false);
 
             if (items.Count > 0 && !items[selectedItem].GetComponent<GrowPlant>().getGrown())
             {
                 items[selectedItem].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-                items[selectedItem].transform.position = transform.position;
-                items[selectedItem].transform.rotation = transform.rotation;
+                items[selectedItem].transform.position = plantLocation.transform.position;
+                items[selectedItem].transform.rotation = plantLocation.transform.rotation;
                 items[selectedItem].GetComponent<GrowPlant>().setGrowing(true);
                 items[selectedItem].gameObject.SetActive(true);
                 items.Remove(items[selectedItem]);
             }
+        }
+
+        if(context.canceled)
+        {
+            plantLocation.SetActive(false);
         }
     }
 
