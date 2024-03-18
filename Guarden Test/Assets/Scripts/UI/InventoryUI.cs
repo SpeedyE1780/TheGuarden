31a687baaa28415ad7c9a4907e595891e6107ea7
Sandro Saab <ss298346@falmouth.ac.uk>
2024-03-18 21:56:27 +0000

    Fix bug when showing UI and selecting item

--- Guarden Test/Assets/Scripts/UI/InventoryUI.cs
+++ Guarden Test/Assets/Scripts/UI/InventoryUI.cs
@@ -6,20 +6,29 @@ public class InventoryUI : MonoBehaviour
 {
     [SerializeField]
     private ItemUI itemPrefab;
+    [SerializeField]
+    private Transform itemParents;
+
 
     public Inventory PlayerInventory { get; set; }
 
     public void FillUI(List<GrowPlant> plants)
     {
-        for (int i = transform.childCount - 1; i >= 0; i--)
+        for (int i = itemParents.childCount - 1; i >= 0; i--)
         {
-            Destroy(transform.GetChild(i).gameObject);
+            Destroy(itemParents.GetChild(i).gameObject);
         }
 
         for (int i = 0; i < plants.Count; i++)
         {
-            ItemUI itemUI = Instantiate(itemPrefab, transform);
-            itemUI.SetItem(plants[i].name, plants[i].GrowthPercentage, () => { PlayerInventory.SelectedItem = i; });
+            ItemUI itemUI = Instantiate(itemPrefab, itemParents);
+            int index = i;
+            itemUI.SetItem(plants[i].name, plants[i].GrowthPercentage, () =>
+            {
+                PlayerInventory.SelectedItem = index;
+                Debug.Log("Selected: " + index);
+                gameObject.SetActive(false);
+            });
         }
     }
 }
