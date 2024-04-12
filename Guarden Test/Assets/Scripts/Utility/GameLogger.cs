a9d796dd19fbfafb02441399c9e8cd4fcc0da629
Sandro Saab <ss298346@falmouth.ac.uk>
2024-04-12 17:38:27 +0100

    Add conditional logging macros

--- Guarden Test/Assets/Scripts/Utility/GameLogger.cs
+++ Guarden Test/Assets/Scripts/Utility/GameLogger.cs
@@ -11,16 +11,19 @@ public static class GameLogger
         Plants = 1 << 2,
     }
 
+    [System.Diagnostics.Conditional("GAME_LOGGER_LOG_INFO")]
     public static void LogInfo(string message, Object sender)
     {
         Debug.Log(message, sender);
     }
 
+    [System.Diagnostics.Conditional("GAME_LOGGER_LOG_WARNING")]
     public static void LogWarning(string message, Object sender)
     {
         Debug.LogWarning(message, sender);
     }
 
+    [System.Diagnostics.Conditional("GAME_LOGGER_LOG_ERROR")]
     public static void LogError(string message, Object sender)
     {
         Debug.LogError(message, sender);
