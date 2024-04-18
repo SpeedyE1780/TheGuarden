ef690df85e2343dc11fcd8eef072da799220b511
Sandro Saab <ss298346@falmouth.ac.uk>
2024-04-18 18:22:37 +0100

    Add TheGuarden.Enemies namespace

--- Guarden Test/Assets/Scripts/Enemies/Enemy.cs
+++ Guarden Test/Assets/Scripts/Enemies/Enemy.cs
@@ -2,158 +2,162 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+namespace TheGuarden.Enemies
 {
-    [SerializeField]
-    private NavMeshAgent agent;
-    [SerializeField]
-    private float patrolSpeed = 5.0f;
-    [SerializeField]
-    private float chaseSpeed = 7.0f;
-    [SerializeField]
-    private float distanceThreshold = 3.0f;
-    [SerializeField]
-    private float detectionRadius = 3.0f;
-    [SerializeField]
-    private LayerMask animalMask;
-    [SerializeField]
-    private Transform holdingPoint;
-    private EnemyPath path;
-    private Animal targetAnimal;
-
-    public EnemyPath Path
+    public class Enemy : MonoBehaviour
     {
-        get
+        [SerializeField]
+        private NavMeshAgent agent;
+        [SerializeField]
+        private float patrolSpeed = 5.0f;
+        [SerializeField]
+        private float chaseSpeed = 7.0f;
+        [SerializeField]
+        private float distanceThreshold = 3.0f;
+        [SerializeField]
+        private float detectionRadius = 3.0f;
+        [SerializeField]
+        private LayerMask animalMask;
+        [SerializeField]
+        private Transform holdingPoint;
+        private EnemyPath path;
+        private Animal targetAnimal;
+
+        public EnemyPath Path
         {
-            return path;
-        }
-        set
-        {
-            path = value;
-        }
-    }
-
-    private bool ReachedDestination => !agent.pathPending && agent.remainingDistance <= distanceThreshold;
-    private bool CanGrabAnimal => targetAnimal != null && Vector3.Distance(transform.position, targetAnimal.transform.position) <= distanceThreshold;
-
-    void Start()
-    {
-        StartCoroutine(Patrol());
-    }
-
-    private Animal DetectAnimal()
-    {
-        Collider[] animals = Physics.OverlapSphere(transform.position, detectionRadius, animalMask);
-
-        foreach (Collider animalCollider in animals)
-        {
-            Animal animal = animalCollider.GetComponent<Animal>();
-
-            if (!animal.InsideForceField)
+            get
             {
-                return animal;
+                return path;
+            }
+            set
+            {
+                path = value;
             }
         }
 
-        return null;
-    }
+        private bool ReachedDestination => !agent.pathPending && agent.remainingDistance <= distanceThreshold;
+        private bool CanGrabAnimal => targetAnimal != null && Vector3.Distance(transform.position, targetAnimal.transform.position) <= distanceThreshold;
 
-    private IEnumerator Patrol()
-    {
-        agent.SetDestination(path.CurrentPosition);
-        agent.speed = patrolSpeed;
-
-        while (true)
+        void Start()
         {
-            if (ReachedDestination)
-            {
-                path.CurrentIndex += 1;
+            StartCoroutine(Patrol());
+        }
 
-                if (path.ReachedEndOfPath)
+        private Animal DetectAnimal()
+        {
+            Collider[] animals = Physics.OverlapSphere(transform.position, detectionRadius, animalMask);
+
+            foreach (Collider animalCollider in animals)
+            {
+                Animal animal = animalCollider.GetComponent<Animal>();
+
+                if (!animal.InsideForceField)
                 {
-                    Destroy(gameObject);
+                    return animal;
+                }
+            }
+
+            return null;
+        }
+
+        private IEnumerator Patrol()
+        {
+            agent.SetDestination(path.CurrentPosition);
+            agent.speed = patrolSpeed;
+
+            while (true)
+            {
+                if (ReachedDestination)
+                {
+                    path.CurrentIndex += 1;
+
+                    if (path.ReachedEndOfPath)
+                    {
+                        Destroy(gameObject);
+                        yield break;
+                    }
+
+                    agent.SetDestination(path.CurrentPosition);
+                }
+
+                targetAnimal = DetectAnimal();
+
+                if (targetAnimal != null)
+                {
+                    StartCoroutine(Chase());
                     yield break;
                 }
 
-                agent.SetDestination(path.CurrentPosition);
+                yield return null;
             }
-
-            targetAnimal = DetectAnimal();
-
-            if (targetAnimal != null)
-            {
-                StartCoroutine(Chase());
-                yield break;
-            }
-
-            yield return null;
         }
-    }
 
-    private IEnumerator Chase()
-    {
-        agent.speed = chaseSpeed;
-        agent.SetDestination(targetAnimal.transform.position);
-
-        while (targetAnimal != null && targetAnimal.Collider.enabled && !targetAnimal.InsideForceField)
+        private IEnumerator Chase()
         {
-            //When animals are in a force field the path ends at the nearest possible point on the edge of the force field
-            //Prevent enemy from holding animal if it can't reach it and it reached the end of the path
-            if (ReachedDestination && CanGrabAnimal)
-            {
-                StartCoroutine(HoldAnimal());
-                yield break;
-            }
-
+            agent.speed = chaseSpeed;
             agent.SetDestination(targetAnimal.transform.position);
-            yield return null;
-        }
 
-        StartCoroutine(Patrol());
-    }
-
-    private IEnumerator HoldAnimal()
-    {
-        targetAnimal.PauseBehavior();
-        agent.SetDestination(transform.position);
-
-        while (targetAnimal.transform.position != holdingPoint.position)
-        {
-            targetAnimal.transform.position = Vector3.MoveTowards(targetAnimal.transform.position, holdingPoint.position, Time.deltaTime * 2);
-            yield return null;
-        }
-
-        StartCoroutine(Escape());
-    }
-
-    private IEnumerator Escape()
-    {
-        agent.SetDestination(path.LastPosition);
-
-        while (true)
-        {
-            targetAnimal.transform.position = holdingPoint.position;
-
-            if (ReachedDestination)
+            while (targetAnimal != null && targetAnimal.Collider.enabled && !targetAnimal.InsideForceField)
             {
-                Destroy(targetAnimal.gameObject);
-                Destroy(gameObject);
+                //When animals are in a force field the path ends at the nearest possible point on the edge of the force field
+                //Prevent enemy from holding animal if it can't reach it and it reached the end of the path
+                if (ReachedDestination && CanGrabAnimal)
+                {
+                    StartCoroutine(HoldAnimal());
+                    yield break;
+                }
+
+                agent.SetDestination(targetAnimal.transform.position);
+                yield return null;
             }
 
-            yield return null;
+            StartCoroutine(Patrol());
         }
-    }
 
-    private void OnDestroy()
-    {
-        if (targetAnimal != null && !targetAnimal.Collider.enabled)
+        private IEnumerator HoldAnimal()
         {
-            targetAnimal.ResumeBehavior();
+            targetAnimal.PauseBehavior();
+            agent.SetDestination(transform.position);
+
+            while (targetAnimal.transform.position != holdingPoint.position)
+            {
+                targetAnimal.transform.position = Vector3.MoveTowards(targetAnimal.transform.position, holdingPoint.position, Time.deltaTime * 2);
+                yield return null;
+            }
+
+            StartCoroutine(Escape());
+        }
+
+        private IEnumerator Escape()
+        {
+            agent.SetDestination(path.LastPosition);
+
+            while (true)
+            {
+                targetAnimal.transform.position = holdingPoint.position;
+
+                if (ReachedDestination)
+                {
+                    Destroy(targetAnimal.gameObject);
+                    Destroy(gameObject);
+                }
+
+                yield return null;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (targetAnimal != null && !targetAnimal.Collider.enabled)
+            {
+                targetAnimal.ResumeBehavior();
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, detectionRadius);
         }
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(transform.position, detectionRadius);
-    }
 }
--- Guarden Test/Assets/Scripts/Enemies/EnemySpawner.cs
+++ Guarden Test/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -3,56 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using TheGuarden.Utility;
 
-public class EnemySpawner : MonoBehaviour
+namespace TheGuarden.Enemies
 {
-    [SerializeField]
-    private Transform spawnPoint;
-    [SerializeField]
-    private List<EnemyPath> paths;
-    [SerializeField]
-    private float startTime;
-    [SerializeField]
-    private float endTime;
-    [SerializeField]
-    private float spawningDelay;
-    [SerializeField]
-    private Enemy enemyPrefab;
-    [SerializeField]
-    private GameTime gameTime;
-
-    private void Start()
+    public class EnemySpawner : MonoBehaviour
     {
-        StartCoroutine(SpawnEnemy());
-    }
+        [SerializeField]
+        private Transform spawnPoint;
+        [SerializeField]
+        private List<EnemyPath> paths;
+        [SerializeField]
+        private float startTime;
+        [SerializeField]
+        private float endTime;
+        [SerializeField]
+        private float spawningDelay;
+        [SerializeField]
+        private Enemy enemyPrefab;
+        [SerializeField]
+        private GameTime gameTime;
 
-    private bool ShouldSpawn()
-    {
-        return gameTime.Hour >= startTime && gameTime.Hour <= endTime;
-    }
-
-    private IEnumerator SpawnEnemy()
-    {
-        while (true)
+        private void Start()
         {
-            yield return new WaitUntil(ShouldSpawn);
+            StartCoroutine(SpawnEnemy());
+        }
 
-            while (ShouldSpawn())
+        private bool ShouldSpawn()
+        {
+            return gameTime.Hour >= startTime && gameTime.Hour <= endTime;
+        }
+
+        private IEnumerator SpawnEnemy()
+        {
+            while (true)
             {
-                Enemy enemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
-                enemy.Path = paths[Random.Range(0, paths.Count)];
-                GameLogger.LogInfo("Enemy Spawned", this, GameLogger.LogCategory.Enemy);
-                yield return new WaitForSeconds(spawningDelay);
+                yield return new WaitUntil(ShouldSpawn);
+
+                while (ShouldSpawn())
+                {
+                    Enemy enemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+                    enemy.Path = paths[Random.Range(0, paths.Count)];
+                    GameLogger.LogInfo("Enemy Spawned", this, GameLogger.LogCategory.Enemy);
+                    yield return new WaitForSeconds(spawningDelay);
+                }
             }
         }
-    }
 
-    private void OnValidate()
-    {
-        gameTime = FindObjectOfType<GameTime>();
-
-        if (gameTime == null)
+        private void OnValidate()
         {
-            GameLogger.LogWarning("Game Time not available in scene", gameObject, GameLogger.LogCategory.Scene);
+            gameTime = FindObjectOfType<GameTime>();
+
+            if (gameTime == null)
+            {
+                GameLogger.LogWarning("Game Time not available in scene", gameObject, GameLogger.LogCategory.Scene);
+            }
         }
-    }
+    } 
 }
