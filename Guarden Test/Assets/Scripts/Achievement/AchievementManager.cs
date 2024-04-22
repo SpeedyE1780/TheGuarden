861e404655f858f86dc189f3daed878041b623bf
Sandro Saab <ss298346@falmouth.ac.uk>
2024-04-22 11:05:34 +0100

    [#53] Add #define to toggle Saving Achievements

--- Guarden Test/Assets/Scripts/Achievement/AchievementManager.cs
+++ Guarden Test/Assets/Scripts/Achievement/AchievementManager.cs
@@ -23,29 +23,33 @@ namespace TheGuarden.Achievements
         /// <summary>
         /// Read save file and set tracker values to saved value or 0
         /// </summary>
-        private void Start()
+        private void LoadAchievementProgress()
         {
+#if THE_GUARDEN_SAVE_ACHIEVEMENTS
+            GameLogger.LogInfo("Loading Achievements from file", this, GameLogger.LogCategory.Achievements);
             string json = FileUtility.ReadFile(AchievementPath);
             AchivementTrackerDictionary achievementsProgress = !string.IsNullOrWhiteSpace(json) ?
                 JsonConvert.DeserializeObject<AchivementTrackerDictionary>(json) :
                 new AchivementTrackerDictionary();
+#else
+            GameLogger.LogInfo("Creating empty achievements dictionary", this, GameLogger.LogCategory.Achievements);
+            AchivementTrackerDictionary achievementsProgress = new AchivementTrackerDictionary();
+#endif
 
             foreach (AchievementTracker tracker in achievementTrackers)
             {
                 tracker.Initialize(achievementsProgress);
-            }
-
-            foreach (Achievement achievement in achievements)
-            {
-                achievement.Initialize();
             }
         }
 
         /// <summary>
         /// Read trackers value and save them to disk
         /// </summary>
-        private void OnDestroy()
+        [System.Diagnostics.Conditional("THE_GUARDEN_SAVE_ACHIEVEMENTS")]
+        private void SaveAchievementProgress()
         {
+            GameLogger.LogInfo("Saving Achievements to file", this, GameLogger.LogCategory.Achievements);
+
             AchivementTrackerDictionary achievementsProgress = new AchivementTrackerDictionary();
 
             foreach (AchievementTracker tracker in achievementTrackers)
@@ -53,14 +57,28 @@ namespace TheGuarden.Achievements
                 tracker.SaveProgress(achievementsProgress);
             }
 
+            string achievementJSON = JsonConvert.SerializeObject(achievementsProgress, Formatting.Indented);
+            FileUtility.WriteFile(AchievementPath, achievementJSON);
+        }
+
+        private void Start()
+        {
+            LoadAchievementProgress();
+
+            foreach (Achievement achievement in achievements)
+            {
+                achievement.Initialize();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            SaveAchievementProgress();
+
             foreach (Achievement achievement in achievements)
             {
                 achievement.Deinitialize();
             }
-
-            string achievementJSON = JsonConvert.SerializeObject(achievementsProgress, Formatting.Indented);
-
-            FileUtility.WriteFile(AchievementPath, achievementJSON);
         }
 
 #if UNITY_EDITOR
@@ -72,7 +90,7 @@ namespace TheGuarden.Achievements
             {
                 if (!achievementTrackers.Contains(achievement.tracker))
                 {
-                    achievementTrackers.Add(achievement.tracker); 
+                    achievementTrackers.Add(achievement.tracker);
                 }
             }
         }
