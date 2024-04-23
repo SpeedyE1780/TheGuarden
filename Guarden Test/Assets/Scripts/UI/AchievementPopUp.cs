07d7acfb26a3e247213dd4ae60dc6c9323b891a7
Sandro Saab <ss298346@falmouth.ac.uk>
2024-04-23 11:38:41 +0100

    Enable queueing completed achievements

--- Guarden Test/Assets/Scripts/UI/AchievementPopUp.cs
+++ Guarden Test/Assets/Scripts/UI/AchievementPopUp.cs
@@ -16,6 +16,15 @@ namespace TheGuarden.UI
         private float speed = 200.0f;
         [SerializeField, Tooltip("Pop up duration on screen")]
         private float duration = 2.0f;
+        [SerializeField, Tooltip("Delay between each pop up")]
+        private float delay = 1.0f;
+
+        private List<Achievement> achievementList = new List<Achievement>();
+
+        private void Start()
+        {
+            StartCoroutine(WaitForAchievement());
+        }
 
         /// <summary>
         /// Called from AchievementManager event
@@ -23,16 +32,34 @@ namespace TheGuarden.UI
         /// <param name="achievement">Completed Achievement</param>
         public void OnAchievementCompleted(Achievement achievement)
         {
-            achievementName.text = achievement.name;
-            StartCoroutine(Popup());
+            achievementList.Add(achievement);
+        }
+
+        /// <summary>
+        /// Enable queueing completed achievement without running multiple pop up coroutine simultaneously
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator WaitForAchievement()
+        {
+            while (true)
+            {
+                yield return new WaitUntil(() => achievementList.Count > 0);
+
+                while (achievementList.Count > 0)
+                {
+                    yield return PopUp(achievementList[0]);
+                    yield return new WaitForSeconds(delay);
+                }
+            }
         }
 
         /// <summary>
         /// Pop up achievement window
         /// </summary>
         /// <returns></returns>
-        private IEnumerator Popup()
+        private IEnumerator PopUp(Achievement achievement)
         {
+            achievementName.text = achievement.name;
             popUpWindow.gameObject.SetActive(true);
             Vector2 startPosition = popUpWindow.anchoredPosition;
 
@@ -51,6 +78,7 @@ namespace TheGuarden.UI
             }
 
             popUpWindow.gameObject.SetActive(false);
+            achievementList.Remove(achievement);
         }
     }
 }
