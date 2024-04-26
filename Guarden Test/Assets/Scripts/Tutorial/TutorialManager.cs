f32fa5d30b84a865b833e28ba3cac044b286095c
Sandro Saab <ss298346@falmouth.ac.uk>
2024-04-26 16:46:55 +0100

    [#97] Tutorial Achievement

--- Guarden Test/Assets/Scripts/Tutorial/TutorialManager.cs
+++ Guarden Test/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TheGuarden.Utility;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace TheGuarden.Tutorial
 {
@@ -9,6 +10,8 @@ namespace TheGuarden.Tutorial
     {
         [SerializeField, Tooltip("List of tutorials")]
         private List<Tutorial> tutorials = new List<Tutorial>();
+
+        public UnityEvent OnTutorialCompleted;
 
         private void Start()
         {
@@ -22,6 +25,9 @@ namespace TheGuarden.Tutorial
                 yield return tutorial.StartTutorial();
                 GameLogger.LogInfo($"{tutorial.name} completed", this, GameLogger.LogCategory.Tutorial);
             }
+
+            GameLogger.LogInfo("TUTORIAL COMPLETED", this, GameLogger.LogCategory.Tutorial);
+            OnTutorialCompleted.Invoke();
         }
     }
 }
