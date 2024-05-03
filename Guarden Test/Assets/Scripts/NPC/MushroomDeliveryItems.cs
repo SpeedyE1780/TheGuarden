fb049a315f63b54f62b5eb324ff5dff3a98c7133
Sandro Saab <ss298346@falmouth.ac.uk>
2024-05-03 18:09:17 +0100

    [#181] Create Mushroom Delivery Item/Items

---
+++ Guarden Test/Assets/Scripts/NPC/MushroomDeliveryItems.cs
@@ -0,0 +1,10 @@
+using TheGuarden.Interactable;
+using UnityEngine;
+
+namespace TheGuarden.NPC
+{
+    [CreateAssetMenu(menuName = "Scriptable Objects/Deliveries/Items List/Mushrooms List")]
+    internal class MushroomDeliveryItems : DeliveryItems<Mushroom>
+    {
+    }
+}
