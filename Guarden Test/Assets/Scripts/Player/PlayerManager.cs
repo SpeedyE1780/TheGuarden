7f02c6fe68f241af8a25ba0f958012f6d7e1954b
Sandro Saab <ss298346@falmouth.ac.uk>
2024-05-07 21:27:20 +0100

    [#194] Optimize switching player control scheme

--- Guarden Test/Assets/Scripts/Player/PlayerManager.cs
+++ Guarden Test/Assets/Scripts/Player/PlayerManager.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.UI;
 using TheGuarden.UI;
 using TheGuarden.Utility;
+using UnityEngine.InputSystem.Users;
+using System.Collections;
 
 namespace TheGuarden.Players
 {
@@ -21,8 +23,17 @@ namespace TheGuarden.Players
         private List<InventoryUI> inventoryUI;
         [SerializeField, Tooltip("List of colors for each player")]
         private List<Color> playerColors;
-        [SerializeField]
-        private int maxPlayerCount = 1;
+
+        private void OnEnable()
+        {
+            InputUser.onUnpairedDeviceUsed += OnUnpairDeviceUsed;
+
+        }
+
+        private void OnDisable()
+        {
+            InputUser.onUnpairedDeviceUsed -= OnUnpairDeviceUsed;
+        }
 
         /// <summary>
         /// OnPlayerJoin is called from the PlayerInputManager component
@@ -30,29 +41,46 @@ namespace TheGuarden.Players
         /// <param name="player">Player who joined the game</param>
         public void OnPlayerJoin(PlayerInput player)
         {
-            if (PlayerInput.all.Count > maxPlayerCount)
-            {
-                bool success = PlayerInput.all[0].SwitchCurrentControlScheme(player.devices.ToArray());
-
-                if (success)
-                {
-                    GameLogger.LogInfo("Switched player 1 control scheme", this, GameLogger.LogCategory.Player);
-                }
-                else
-                {
-                    GameLogger.LogError("Unable to switch player 1 control scheme", this, GameLogger.LogCategory.Player);
-                }
-
-                Destroy(player.gameObject);
-                return;
-            }
-
             player.camera = followCamera.Camera;
             PlayerController controller = player.GetComponent<PlayerController>();
             int playerIndex = player.playerIndex == -1 ? 0 : player.playerIndex;
             controller.SetColor(playerColors[playerIndex]);
             controller.Inventory.SetInventoryUI(inventoryUI[playerIndex]);
             followCamera.AddTarget(player.transform);
+        }
+
+        private void OnUnpairDeviceUsed(InputControl arg1, UnityEngine.InputSystem.LowLevel.InputEventPtr arg2)
+        {
+            StartCoroutine(SwitchControlScheme(arg1.device));
+        }
+
+        private IEnumerator SwitchControlScheme(InputDevice device)
+        {
+            //Wait one frame in case this call triggers a player join in the player input manager
+            yield return null;
+
+            if (PlayerInput.FindFirstPairedToDevice(device) != null)
+            {
+                GameLogger.LogWarning($"Device {device.name} already paired", this, GameLogger.LogCategory.Player);
+                yield break;
+            }
+
+            if (PlayerInput.all.Count == 0)
+            {
+                GameLogger.LogWarning($"No player available to pair with {device.name}", this, GameLogger.LogCategory.Player);
+                yield break;
+            }
+
+            bool success = PlayerInput.all[0].SwitchCurrentControlScheme(device);
+
+            if (success)
+            {
+                GameLogger.LogInfo($"Switched player 1 control scheme to {device.name}", this, GameLogger.LogCategory.Player);
+            }
+            else
+            {
+                GameLogger.LogError($"Unable to switch player 1 control scheme to {device.name}", this, GameLogger.LogCategory.Player);
+            }
         }
 
         /// <summary>
