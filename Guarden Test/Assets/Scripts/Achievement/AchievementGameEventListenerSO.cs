d38b6ea6bef00bf77b8c64b1f9eaf7e4ae4a975d
Sandro Saab <ss298346@falmouth.ac.uk>
2024-05-10 10:19:18 +0100

    Add Achievements Documentation

--- Guarden Test/Assets/Scripts/Achievement/AchievementGameEventListenerSO.cs
+++ Guarden Test/Assets/Scripts/Achievement/AchievementGameEventListenerSO.cs
@@ -1,8 +1,11 @@
-using UnityEngine;
 using TheGuarden.Utility.Events;
+using UnityEngine;
 
 namespace TheGuarden.Achievements
 {
+    /// <summary>
+    /// Scriptable Objects that listens to an AchievementGameEvent
+    /// </summary>
     [CreateAssetMenu(menuName = "Scriptable Objects/Game Events/Listeners/<Achievement>")]
     internal class AchievementGameEventListenerSO : TGameEventListenerSO<Achievement> { }
 }
--- Guarden Test/Assets/Scripts/Achievement/AchievementlGameEventListener.cs
+++ Guarden Test/Assets/Scripts/Achievement/AchievementlGameEventListener.cs
@@ -2,5 +2,8 @@ using TheGuarden.Utility.Events;
 
 namespace TheGuarden.Achievements
 {
+    /// <summary>
+    /// Monobehavior component that listens to an Achievement Game Event
+    /// </summary>
     public class AchievementGameEventListener : TGameEventListener<Achievement> { }
 }
