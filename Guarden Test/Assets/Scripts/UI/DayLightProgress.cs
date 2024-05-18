fb9a29dba73f8ad5fa8c3f00c22cc5e4fa394af4
Sandro Saab <ss298346@falmouth.ac.uk>
2024-05-18 11:55:20 +0100

    [#245] Show instructions to start wave early

--- Guarden Test/Assets/Scripts/UI/DayLightProgress.cs
+++ Guarden Test/Assets/Scripts/UI/DayLightProgress.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using TheGuarden.Utility;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,9 +15,49 @@ namespace TheGuarden.UI
         private Slider dayProgress;
         [SerializeField, Tooltip("DayLight cycle in scene")]
         private DayLightCycle cycle;
+        [SerializeField, Tooltip("Text showing start wave instructions")]
+        private TextMeshProUGUI instructionText;
+        [SerializeField, Tooltip("StartWave early instruction text")]
+        private InteractionInstruction instruction;
 
-        private void Update()
+        private bool dayCycle = false;
+
+        private void Start()
         {
+            instructionText.text = instruction.GetAllBindingsInstructionMessage();
+        }
+
+        /// <summary>
+        /// Called from game event
+        /// </summary>
+        public void OnDayStarted()
+        {
+            dayCycle = true;
+            instructionText.gameObject.SetActive(true);
+            StartCoroutine(ShowDayInstructions());
+        }
+
+        /// <summary>
+        /// Called from game event
+        /// </summary>
+        public void OnNightStarted()
+        {
+            dayCycle = false;
+            instructionText.gameObject.SetActive(false);
+        }
+
+        /// <summary>
+        /// Update slider during day cycle
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator ShowDayInstructions()
+        {
+            while (dayCycle)
+            {
+                dayProgress.value = cycle.DayProgess;
+                yield return null;
+            }
+
             dayProgress.value = cycle.DayProgess;
         }
     }
